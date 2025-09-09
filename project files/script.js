63592e856224959ecf3c97d7856911cc411b8c0e
M. Navadeep <m.navdeep.rl9@gmail.com>
2025-09-09 20:14:32 +0530

    Delete project files directory

--- project files/script.js
+++
@@ -1,6 +0,0 @@
-const fileInput = document.getElementById('butterflyImage');
-const fileName = document.getElementById('fileName');
-
-fileInput?.addEventListener('change', function () {
-    fileName.textContent = this.files.length > 0 ? this.files[0].name : 'No file chosen';
-});
